a5aa90e4e43ffa2d2b0a93956335c0f367bef15b
crissdua <crissdua@hotmail.com>
2018-03-08 15:57:53 -0600

    theme finalizado

--- Produccion_F2/FrmP.Designer.vb
+++ Produccion_F2/FrmP.Designer.vb
@@ -26,36 +26,41 @@ Partial Class FrmP
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label13 = New System.Windows.Forms.Label()
         Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.CHK = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.BarCodes = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.txtOrder = New System.Windows.Forms.TextBox()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.CHK = New System.Windows.Forms.DataGridViewCheckBoxColumn()
-        Me.BarCodes = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(195, 258)
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button3.BackgroundImage = Global.Produccion_F2.My.Resources.Resources.button
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button3.Location = New System.Drawing.Point(202, 357)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(89, 23)
+        Me.Button3.Size = New System.Drawing.Size(67, 23)
         Me.Button3.TabIndex = 71
         Me.Button3.Text = "Salir"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(100, 258)
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackgroundImage = Global.Produccion_F2.My.Resources.Resources.cancelar
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button2.Location = New System.Drawing.Point(107, 357)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(89, 23)
+        Me.Button2.Size = New System.Drawing.Size(67, 23)
         Me.Button2.TabIndex = 70
-        Me.Button2.Text = "Cancelar"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Label13
         '
         Me.Label13.AutoSize = True
-        Me.Label13.Location = New System.Drawing.Point(59, 14)
+        Me.Label13.Location = New System.Drawing.Point(60, 27)
         Me.Label13.Name = "Label13"
         Me.Label13.Size = New System.Drawing.Size(49, 13)
         Me.Label13.TabIndex = 67
@@ -65,39 +70,15 @@ Partial Class FrmP
         '
         Me.DGV.AllowUserToAddRows = False
         Me.DGV.AllowUserToDeleteRows = False
+        Me.DGV.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DGV.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.CHK, Me.BarCodes})
-        Me.DGV.Location = New System.Drawing.Point(47, 56)
+        Me.DGV.Location = New System.Drawing.Point(47, 79)
         Me.DGV.Name = "DGV"
         Me.DGV.ReadOnly = True
-        Me.DGV.Size = New System.Drawing.Size(197, 196)
+        Me.DGV.Size = New System.Drawing.Size(201, 272)
         Me.DGV.TabIndex = 66
-        '
-        'txtOrder
-        '
-        Me.txtOrder.Location = New System.Drawing.Point(62, 30)
-        Me.txtOrder.Name = "txtOrder"
-        Me.txtOrder.Size = New System.Drawing.Size(100, 20)
-        Me.txtOrder.TabIndex = 72
-        '
-        'Button4
-        '
-        Me.Button4.Location = New System.Drawing.Point(5, 258)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(89, 23)
-        Me.Button4.TabIndex = 73
-        Me.Button4.Text = "Aceptar"
-        Me.Button4.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.Button1.Location = New System.Drawing.Point(168, 27)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(59, 23)
-        Me.Button1.TabIndex = 69
-        Me.Button1.Text = "Buscar"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'CHK
         '
@@ -112,11 +93,41 @@ Partial Class FrmP
         Me.BarCodes.Name = "BarCodes"
         Me.BarCodes.ReadOnly = True
         '
+        'txtOrder
+        '
+        Me.txtOrder.Location = New System.Drawing.Point(63, 43)
+        Me.txtOrder.Name = "txtOrder"
+        Me.txtOrder.Size = New System.Drawing.Size(100, 20)
+        Me.txtOrder.TabIndex = 72
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button4.BackgroundImage = Global.Produccion_F2.My.Resources.Resources.crear
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button4.Location = New System.Drawing.Point(12, 357)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(67, 23)
+        Me.Button4.TabIndex = 73
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.Location = New System.Drawing.Point(169, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(59, 23)
+        Me.Button1.TabIndex = 69
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'FrmP
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 293)
+        Me.BackgroundImage = Global.Produccion_F2.My.Resources.Resources.fondoGrande
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(300, 404)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.txtOrder)
         Me.Controls.Add(Me.Button3)
@@ -124,6 +135,7 @@ Partial Class FrmP
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label13)
         Me.Controls.Add(Me.DGV)
+        Me.DoubleBuffered = True
         Me.Name = "FrmP"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Traslado Stock"
